7be2c12226397f6a3c705a92ee177fa9f83e422a
mehmetkayikcii <06mehmetkayikci@gmail.com>
2025-03-24 15:47:35 +0300

    Api Controller Oluşturma

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieByIdQuerHandler.cs
@@ -0,0 +1,38 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries;
+using MovieApi.Application.Features.CQRSDesignPattern.Result.MovieResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class GetMovieByIdQuerHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetMovieByIdQuerHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetMovieByIdQueryResult> Handle(GetMovieByIdQuery query)
+        {
+            var value =await _context.Movies.FindAsync(query.MovieId);
+            return new GetMovieByIdQueryResult
+            {
+                CoverImageUrl = value.CoverImageUrl,
+                CreatedYear = value.CreatedYear,
+                Description = value.Description,
+                Duration = value.Duration,
+                Rating = value.Rating,
+                ReleaseDate = value.ReleaseDate,
+                Status = value.Status,
+                Title = value.Title
+
+            };
+        }
+    }
+}
--- Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.CategoryCommands;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
 
 namespace MovieApi.WebApi.Controllers
 {
@@ -7,6 +9,33 @@ namespace MovieApi.WebApi.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-       // private readonly GetCategoryQueryHandler
-    }
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly GetCategoryByIdQueryHandler getCategoryByIdQueryHandler;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+
+        public CategoriesController(GetCategoryQueryHandler getCategoryQueryHandler, GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler)
+        {
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            this.getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var value = await _getCategoryQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+            await _createCategoryCommandHandler.Handle(command);
+            return Ok();
+        }
+    } 
 }
